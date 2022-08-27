c329042e3ab6b6ce60ba959a1d73095d4022368f
gilcllys <gilcllyys.costa@fpf.br>
2022-08-26 21:37:10 -0400

    Projeto de flash card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import {Component,ViewChild} from '@angular/core';
+import {IFlash} from "./flash.model";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'ng-flascard';
+  editing:boolean = false
+  question:string=''
+  answer:string = ''
+  public getRandomNumber() {
+    return Math.floor(Math.random() * 10000)
+  }
+
+  flashes: IFlash[] = [
+    {
+      question: 'Question 1',
+      answer: 'Answer: 1',
+      show: false,
+      id: this.getRandomNumber()
+    },
+    {
+      question: 'Question 2',
+      answer: 'Answer: 2',
+      show: false,
+      id: this.getRandomNumber()
+    },
+    {
+      question: 'Question 3',
+      answer: 'Answer: 3',
+      show: false,
+      id: this.getRandomNumber()
+    }]
+
+  public handleToogleCard(event: any): void {
+    event.show = !event.show
+  }
+
+  public handleDelete(event:any) {
+    const flashId = this.flashes.indexOf(event);
+    console.log(flashId)
+    this.flashes.splice(flashId, 1)
+  }
+
+  handleEdit(event:any){
+
+    //TODO: we will add aditing logic after adding the form
+  }
+
+  handleRemenberChange(event:any){
+    event.flash.remembered = event.flag
+  }
+
+  onSubmit(f:NgForm){
+    if(f.valid){
+      this.flashes.push({
+        question: f.value.question,
+        answer: f.value.answer,
+        show: false,
+        id: this.getRandomNumber()
+      })
+    }
+
+  }
+
+  onClear():void{
+    this.question = ''
+    this.answer = ''
+  }
+
+
 }
